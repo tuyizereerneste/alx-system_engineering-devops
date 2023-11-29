da09501f1637c470acf5a9b8962d84429109d22a
tuyizereerneste <tuyizereernest@yahoo.com>
2023-11-29 19:24:21 +0200

    configure Nginx server using puppet update

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -11,6 +11,9 @@ exec {'Hello World!':
   command  => 'echo "Hello World!" | sudo dd status=none of=/var/www/html/index.html',
   provider => shell,
 }
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/doretest.tech\/;\\n\\t}/" /etc/nginx/sites-available/default':
+  provider => shell,
+}
 exec {'run':
   command  => 'sudo service nginx restart',
   provider => shell,
