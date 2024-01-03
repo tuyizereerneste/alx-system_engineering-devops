53027e399a6a1bbbc11c17eb37cd2a85df21df1f
tuyizereerneste <tuyizereernest@yahoo.com>
2024-01-04 00:53:11 +0200

    export data in json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Python script to export data in the JSON format."""
+import json
 import requests
 import sys
-import json
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
